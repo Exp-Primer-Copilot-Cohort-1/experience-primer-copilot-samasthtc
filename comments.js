8f027548dc5642a505f8119c323f5884eb2b9623
samasthtc <79966712+samasthtc@users.noreply.github.com>
2023-09-25 21:53:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -11,7 +11,7 @@ router.post('/create', commentController.create);
 
 // router for deleting comment
 router.get('/destroy/:id', commentController.destroy);
-
+  
 // export router
 module.exports = router;
 
