43a3d10b39f50167958e3e564e059559855f90ad
samasthtc <79966712+samasthtc@users.noreply.github.com>
2023-09-25 21:57:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,37 @@
-// create web server for comment
-// import express
+// Create web server
+
 const express = require('express');
-// create router
 const router = express.Router();
-// import comment controller
-const commentController = require('../controllers/comments_controller');
+const commentsController = require('../controllers/commentsController')
+const {check} = require('express-validator');
+const auth = require('../middleware/auth');
 
-// router for creating comment
-router.post('/create', commentController.create);
+// Add comment
+// api/comments
+router.post('/', 
+    auth,
+    [
+        check('comment', 'Comment is required').not().isEmpty()
+    ],
+    commentsController.addComment
+);
 
-// router for deleting comment
-router.get('/destroy/:id', commentController.destroy);
-  
-// export router
+// Get all comments
+router.get('/',
+    auth,
+    commentsController.getComments
+);
+
+// Update comment
+router.put('/:id',
+    auth,
+    commentsController.updateComment
+);
+
+// Delete comment
+router.delete('/:id',
+    auth,
+    commentsController.deleteComment
+);
+
 module.exports = router;
-
