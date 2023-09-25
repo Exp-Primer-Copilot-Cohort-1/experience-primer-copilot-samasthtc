da71e29081290a0438cd38a5f04a3adb13e9c2e4
samasthtc <79966712+samasthtc@users.noreply.github.com>
2023-09-25 21:52:13 +0000

    created comments.js

--- comments.js
+++ comments.js
@@ -1,6 +1,17 @@
 // create web server for comment
+// import express
+const express = require('express');
+// create router
+const router = express.Router();
+// import comment controller
+const commentController = require('../controllers/comments_controller');
 
-// 1. create web server
-var http = require('http');
-var url = require('url');
-var qs = require('querystring');
+// router for creating comment
+router.post('/create', commentController.create);
+
+// router for deleting comment
+router.get('/destroy/:id', commentController.destroy);
+
+// export router
+module.exports = router;
+
